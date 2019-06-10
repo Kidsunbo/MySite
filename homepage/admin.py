a4da8013cf9e2a4c16ca1903ea0c22bd93ba83b8
Bo Sun (MSc Computer Science FT) <qiongxiaozi158@sina.com>
2019-06-10 17:21:05 +0100

    first setup

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import  Article
+from django.db import models
+from tinymce.widgets import TinyMCE
+
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
     fieldsets = [("Title & Date",{"fields":["title","pub_date"]}),
                  ("Main Content",{"fields":["content"]})]
+
+    formfield_overrides = {
+        models.TextField:{'widget':TinyMCE()}
+    }
 
 admin.site.register(Article,ArticleAdmin)
