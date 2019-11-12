9712a78a020d7c610b8e19cd213f928c101b341f
Kie <qiongxiaozi158@sina.com>
2019-11-12 14:24:45 +0800

    add ray's web app

--- app/urls.py
+++ app/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("homepage.urls"))
+    path('developing',include("homepage.urls")),
+    path('',include("ray.urls")),
 ]
