3cb67ae06ffa2a48780e08acd147ebbfcdf737a8
Kie <qiongxiaozi158@sina.com>
2020-01-07 23:53:43 +0800

    Revert "add ray's web app"
    This reverts commit 9712a78a020d7c610b8e19cd213f928c101b341f.

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('developing',include("homepage.urls")),
-    path('',include("ray.urls")),
+    path('',include("homepage.urls"))
 ]
