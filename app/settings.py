66467ea7c106d2f5ef1707d8d8d1945494b133f9
Kie <qiongxiaozi158@sina.com>
2020-03-29 19:25:58 +0800

    Change debug mode to release mode

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0kdvay78nt*i8q_+4j_!-5954j2^&2tavus#p@@+=8^*n@a!ll'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kies.herokuapp.com','*']
 
