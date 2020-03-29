8aa996893ebc1e2d17ee0e9066c841ff44268fc4
Kie <qiongxiaozi158@sina.com>
2020-03-29 16:51:24 +0800

    update something

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0kdvay78nt*i8q_+4j_!-5954j2^&2tavus#p@@+=8^*n@a!ll'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kies.herokuapp.com','*']
 
